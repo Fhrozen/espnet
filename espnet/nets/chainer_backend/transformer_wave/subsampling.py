2fb0be8e3e33d3076106df5c67653666f0ab1b60
Fhrozen <nyalta21@gmail.com>
2020-04-25 11:52:22 +0900

    fix res learn

--- espnet/nets/chainer_backend/transformer_wave/subsampling.py
+++ espnet/nets/chainer_backend/transformer_wave/subsampling.py
@@ -270,7 +270,6 @@ class StftResLearn(chainer.Chain):
                  hop_length=160):
         super(StftResLearn, self).__init__()
         import librosa
-        # Ref: https://github.com/pseeth/pytorch-stft/blob/master/stft.py#L33
         self.filter_length = filter_length
         self.hop_length = hop_length
         logging.info('Stft param with stft initializer')
@@ -284,8 +283,8 @@ class StftResLearn(chainer.Chain):
                             fmax=None)
         self.dropout = dropout
         melmat = librosa.filters.mel(**_mel_options).T
-        self.fourier_basis = fourier_basis[:, None, None, :]
         self.melmat = melmat.astype(np.float32)
+        self.register_persistent('melmat')
         with self.init_scope():
             self.norm = L.GroupNormalization(1, mels)
             n = 1 * 3 * 3
@@ -303,12 +302,12 @@ class StftResLearn(chainer.Chain):
             self.out = L.Linear(idim, dims, initialW=chainer.initializers.Uniform(scale=stvd),
                             initial_bias=chainer.initializers.Uniform(scale=stvd))
             self.pe = PositionalEncoding(dims, dropout)
+            self.fourier_basis = chainer.Parameter(fourier_basis[:, None, None, :])
 
     def __call__(self, xs, ilens):
-        xp = self.xp
-        xs = F.expand_dims(xs, axis=1).data
+        xs = F.expand_dims(self.xp.array(xs), axis=1).data
         # BS x 1 x T x NFFT
-        xs = F.convolution_2d(xp.array(xs), xp.array(self.fourier_basis), stride=1, pad=0)
+        xs = F.convolution_2d(xs, self.fourier_basis, stride=1, pad=0)
         # BS x NFFT/2+1 * 2 x T x 1 
         xs = F.squeeze(xs, axis=3)
         cutoff = int((self.filter_length / 2) + 1)
@@ -317,17 +316,17 @@ class StftResLearn(chainer.Chain):
         xs = real_xs ** 2 + imag_xs ** 2
 
         xs = F.swapaxes(xs, 1, 2)
-        xs = F.matmul(xs, xp.array(self.melmat))
+        xs = F.matmul(xs, self.melmat)
         # BS x T x MEL
         xs = F.log(F.absolute(xs) + 1e-20).transpose(0, 2, 1)
         # Norm
-        xs = self.norm(xs.data).transpose(0, 2, 1)
+        xs = self.norm(xs)
         xs = F.expand_dims(xs, axis=1)
         xs = self.conv0(xs)
         xs = self.conv1(xs)
         xs = self.conv2(xs)
-        batch, _, length, _ = xs.shape
-        xs = self.out(F.swapaxes(xs, 1, 2).reshape(batch * length, -1))
+        batch, _, _, length = xs.shape
+        xs = self.out(xs.transpose(0, 3, 1, 2).reshape(batch * length, -1))
         xs = self.pe(xs.reshape(batch, length, -1))
         # change ilens accordingly
         ilens = np.ceil(np.array(ilens, dtype=np.float32) / 2).astype(np.int)
