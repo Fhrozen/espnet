9f5133ee479e8a2385cbe6b86e78e60269e94692
Nelson Yalta <nyalta21@gmail.com>
2020-11-07 20:50:38 +0900

    Add reading sequence code

--- onnx/samples/speech_recog_offline/speech_recog_offline/Program.cs
+++ onnx/samples/speech_recog_offline/speech_recog_offline/Program.cs
@@ -6,6 +6,11 @@ using Microsoft.ML.OnnxRuntime.Tensors;
 using System.Text;
 using CommandLine;
 using CommandLine.Text;
+using Microsoft.ML.OnnxRuntime;
+using Serilog;
+using Serilog.Core;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
 
 
 namespace speech_recog_offline
@@ -22,13 +27,9 @@ namespace speech_recog_offline
               HelpText = "Input file to be processed.")]
             public string InputFile { get; set; }
 
-            //[Option('o', "output", Required = true,
-            //  HelpText = "Output file to be processed.")]
-            //public string OutputFile { get; set; }
-
-            [Option('v', "verbose", Default = true,
+            [Option('v', "verbose", Default = 1,
               HelpText = "Prints all messages to standard output.")]
-            public bool Verbose { get; set; }
+            public int Verbose { get; set; }
 
             [Option("samplerate", Default = 16000)]
             public float Samplerate { get; set; }
@@ -53,25 +54,68 @@ namespace speech_recog_offline
                     break;
             }
 
+            // Logger
+            int Verbose = opts.Verbose;
+            var levelSwitch = new LoggingLevelSwitch();
+            switch (Verbose)
+            {
+                case 0:
+                    levelSwitch.MinimumLevel = Serilog.Events.LogEventLevel.Debug;
+                    break;
+                case 1:
+                    levelSwitch.MinimumLevel = Serilog.Events.LogEventLevel.Information;
+                    break;
+                default:
+                    levelSwitch.MinimumLevel = Serilog.Events.LogEventLevel.Warning;
+                    break;
+            }
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.ControlledBy(levelSwitch)
+                .WriteTo.Console()
+                .CreateLogger();
+            Log.Warning($"Level of Logging: {levelSwitch.MinimumLevel}");
+
             // Read paths
             string modelFilePath = opts.ModelFile;
             string audioFilePath = opts.InputFile;
+            string FileName = "";
+            string Format = "";
 
-            // Preprocess image
-            var paddedHeight = (int)(Math.Ceiling(120 / 32f) * 32f);
-            var paddedWidth = (int)(Math.Ceiling(120 / 32f) * 32f);
+            if (audioFilePath.Contains(":"))
+            {
+                FileName = audioFilePath.Substring(audioFilePath.IndexOf(':') + 1);
+                Format = audioFilePath.Split(':')[0];
+                Log.Information($"opt.Format = {Format}");
+            }
+            else
+            {
+                Log.Error("ERROR: Input file should follow format scp:<list_of_files> or wav:<filename.wav>");
+                System.Environment.Exit(1);
+            }
 
-            Console.WriteLine($"opt.A = {opts.ModelFile}");
-            Console.WriteLine($"opt.A = {opts.InputFile}");
-            Console.WriteLine($"opt.A = {opts.Samplerate}");
+            // Print Options
+            Log.Debug($"opt.ModelFile = {opts.ModelFile}");
+            Log.Debug($"opt.InputFile = {opts.InputFile}");
+            Log.Debug($"opt.Samplerate = {opts.Samplerate}");
+
+            // Read Input
+            float[] ThisInput = GetWav(FileName);
+            Log.Debug($"Length of input = {ThisInput.Length}");
+
+            // Run Inference
+            using var session = new InferenceSession(modelFilePath);
+
+
         }
-        static void RunOptions(Options opts)
+
+        static float[] GetWav(string ThisFile)
         {
-            //handle options
-        }
-        static void HandleParseError(IEnumerable<Error> errs)
-        {
-            //handle errors
+            float[] ThisFloatBuffer;
+            AudioFileReader ThisReader = new AudioFileReader(ThisFile);
+            ISampleProvider ThisProvider = ThisReader.ToSampleProvider();
+            ThisFloatBuffer = new float[ThisReader.Length / 2];
+            ThisProvider.Read(ThisFloatBuffer, 0, ThisFloatBuffer.Length);
+            return ThisFloatBuffer;
         }
     }
 }
